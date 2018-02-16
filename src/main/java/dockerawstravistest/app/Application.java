b4f55cc22aea67f6b9d70d2e1040691353de8d1c
Yurii Sych <sichyuriyy@gmail.com>
2018-02-16 17:18:37 +0200

    Added all app beans log

--- src/main/java/dockerawstravistest/app/Application.java
+++ src/main/java/dockerawstravistest/app/Application.java
@@ -1,10 +1,15 @@
 package dockerawstravistest.app;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @PropertySource("classpath:app.properties")
@@ -13,5 +18,20 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 }
